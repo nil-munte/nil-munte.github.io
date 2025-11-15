bf8ddfe8cf2f149426af589b88b4486f777a3453
Nil Munté Guerrero <nilmunte@pc-2021.home>
2025-11-16 00:38:09 +0100

    Modifying astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-site: 'https://nil-munte.github.io',
-integrations: [tailwind()],
+  site: 'https://nil-munte.github.io',
+  integrations: [tailwind()],
+  output: "static",
 });
